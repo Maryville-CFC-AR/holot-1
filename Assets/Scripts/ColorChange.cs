56a5781dc925eb09d2d7733dd50900a83bf44f28
jmcgee1 <31488137+jmcgee1@users.noreply.github.com>
2018-02-21 17:06:44 -0600

    added pill project

---
+++ Assets/Scripts/ColorChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour {
+    int counter = 0;
+	void Update () {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            counter = counter + 1;
+            if (counter % 2 != 0)
+            {
+                GetComponent<Renderer>().material.color = Color.blue;
+            } else
+            {
+                GetComponent<Renderer>().material.color = Color.red;
+            }
+        }
+	}
+}
